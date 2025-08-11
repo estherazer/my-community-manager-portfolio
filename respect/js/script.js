0269d88f512e501cacaf21dd63ed19f86cda2583
Esther OUEDRAOGO <estherazerzera@gmail.com>
2025-08-11 23:32:34 +0000

    Add files via upload

---
+++ respect/js/script.js
@@ -0,0 +1,14 @@
+function filterProjects(category) {
+    const projects = document.querySelectorAll('.project');
+    projects.forEach(project => {
+        if (category === 'all') {
+            project.style.display = 'block';
+        } else {
+            if (project.classList.contains(category)) {
+                project.style.display = 'block';
+            } else {
+                project.style.display = 'none';
+            }
+        }
+    });
+}
